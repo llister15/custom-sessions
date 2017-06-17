868d80429b9b0650815414387d0a14176597b6a2
Louis Lister <llister15@users.noreply.github.com>
2017-06-17 04:12:35 -0700

    Update custom-sessions.php

--- custom-sessions.php
+++ custom-sessions.php
@@ -9,10 +9,6 @@ Author URI: http://wonkasoft.com
 License: GPL2
 */
 
-add_action('init', 'register_my_session', 1);
-add_action('wp_logout', 'myEndSession');
-add_action('wp_login', 'myEndSession');
-add_action('wp_head', 'header_config');
 
 // Setup session
 function register_my_session() {
@@ -21,32 +17,45 @@ function register_my_session() {
     session_start();
   }
 }
+add_action('init', 'register_my_session', 1);
 
 // End session
 function myEndSession() {
   session_destroy ();
 }
+add_action('wp_logout', 'myEndSession');
 
 // Set session variable for rep number and discount number
 // Validate by parameter
 // If repnum is not set then the user will be redirected to the 404 page
 function header_config() {
 
+// Set vaild rep numbers
+$allowed_repnum = array('100','101','102','103','104','105','106','107','108');
+
 // Validation by repnum
 // if valid then set session variable for repnum
-  if (!$_SESSION['repnum'] && $_GET['repnum'] != '') {
+  if (!$_SESSION['repnum'] && $_GET['repnum'] != '' && in_array($_GET['repnum'], $allowed_repnum)) {
     $_SESSION['repnum'] = $_GET['repnum'];
-  } elseif ($_SESSION['repnum'] == '' && $_GET['repnum'] =='') {
-          status_header( 404 );
+  } elseif (!$_SESSION['repnum'] && $_GET['repnum'] =='') {
+          session_destroy ();
+          status_header( 401 );
           nocache_headers();
-          include( get_query_template( '404' ) );
+          include( get_query_template( 'norep' ) );
           die();
     }
 
 // If new parameter sent update session variable for repnum
   if ($_SESSION['repnum'] != $_GET['repnum'] && $_GET['repnum'] != '') {
     $_SESSION['repnum'] = $_GET['repnum'];
-  }
+    if (!in_array($_SESSION['repnum'], $allowed_repnum)) {
+      session_destroy();
+      status_header( 401 );
+      nocache_headers();
+      include( get_query_template( 'norep' ) );
+      die();
+    }
+  } 
 
 // Set discount variable by parameter
   if (!$_SESSION['discount'] && $_GET['discount']!=''){
@@ -59,10 +68,10 @@ function header_config() {
   }
 
 }
+add_action('wp_head', 'header_config');
+
 
 // Add rep number to woocommerce checkout form from session variable
-add_filter( 'woocommerce_checkout_fields' , 'add_repnum_to_checkout' );
-
 function add_repnum_to_checkout( $fields ) {
 
     $repnum_session = $_SESSION['repnum'];
@@ -73,5 +82,5 @@ function add_repnum_to_checkout( $fields ) {
 
     return $fields;
 }
-
+add_filter( 'woocommerce_checkout_fields' , 'add_repnum_to_checkout' );
 ?>
